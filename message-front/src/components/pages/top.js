35f920d99a72afdeeeeaad1547d2298d855105c4
mizumura3 <mizsato1014@gmail.com>
2018-09-23 16:28:36 +0900

    fix layout

--- message-front/src/components/pages/top.js
+++ message-front/src/components/pages/top.js
@@ -3,21 +3,57 @@ import { Link } from "@hyperapp/router";
 
 export default () => (state, actions) => (
   <main>
-    <div>
-      <Link to={`/upload`}>アップロード</Link>
+    <nav
+      class="navbar has-shadow is-spaced"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="http://localhost:1234">
+          <div>LGTM</div>
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="http://localhost:1234">
+            Home
+          </a>
+          <a class="navbar-item" href="http://localhost:1234">
+            Submit
+          </a>
+          <a class="navbar-item" href="http://localhost:1234">
+            Random
+          </a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="level-item">
+      <div class="field has-addons">
+        <p class="control">
+          <button class="button" onclick={actions.items}>
+            表示
+          </button>
+        </p>
+      </div>
     </div>
-    <div>
-      <button class="button" onclick={actions.items}>
-        表示
-      </button>
-    </div>
+
     <div>
       {state.items.map(item => (
         <img src={item} width="200" height="200" />
       ))}
-    </div>
-    <div class="simple-form-field">
-      <div class="siimple-btn siimple-btn--blue">Send comment</div>
     </div>
   </main>
 );
