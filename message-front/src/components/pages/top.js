4f484cae2910ad7e5ddfc230fa097b26ee702b23
mizumura3 <mizsato1014@gmail.com>
2018-09-30 20:58:56 +0900

    fix layout

--- message-front/src/components/pages/top.js
+++ message-front/src/components/pages/top.js
@@ -3,10 +3,30 @@ import { Link } from "@hyperapp/router";
 
 export default () => (state, actions) => (
   <main>
-    <div>
-      {state.items.map((item, index) => (
-        <img key={index} src={item} width="200" height="200" />
-      ))}
+    <div class="container">
+      <div class="columns is-multiline">
+        {state.items.map((item, index) => (
+          <div class="column is-one-quarter">
+            <img key={index} src={item} width="200" height="200" />
+          </div>
+        ))}
+      </div>
     </div>
+
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Bulma</strong> by{" "}
+          <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
+          licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
+          website content is licensed{" "}
+          <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+            CC BY NC SA 4.0
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
   </main>
 );
