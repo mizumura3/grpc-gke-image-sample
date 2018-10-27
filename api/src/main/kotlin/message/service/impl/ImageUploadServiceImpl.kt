0aa3d0e0ccb91555c417bcb65fe2e92d6f48394e
mizumura3 <mizsato1014@gmail.com>
2018-10-27 17:06:21 +0900

    move package

--- api/src/main/kotlin/message/service/impl/ImageUploadServiceImpl.kt
+++ api/src/main/kotlin/message/service/impl/ImageUploadServiceImpl.kt
@@ -1,12 +1,19 @@
 package message.service.impl
 
+import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.cloud.storage.StorageOptions
 import message.properties.GcpProperties
 import message.service.ImageUploadService
 import org.springframework.stereotype.Service
+import java.io.FileInputStream
 
 @Service
 class ImageUploadServiceImpl(private val gcpProperties: GcpProperties) : ImageUploadService {
     override fun upload(name: String, byteArray: ByteArray) {
-        println(gcpProperties.secretKey)
+        val hoge = StorageOptions.newBuilder()
+                .setCredentials(ServiceAccountCredentials.fromStream(FileInputStream(gcpProperties.secretKey)))
+                .build()
+                .service
+        print(hoge)
     }
 }
