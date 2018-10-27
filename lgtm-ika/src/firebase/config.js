b7b62628f0f36d37b1bc5a994eeb44a1a9a38ab8
mizumura3 <mizsato1014@gmail.com>
2018-10-27 18:00:17 +0900

    add firebase api key

---
+++ lgtm-ika/src/firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDzRIzBBrBFrCj3tVSoUiEDCHZdFjMTGUE",
+  authDomain: "lgtm-ika.firebaseapp.com",
+  databaseURL: "https://lgtm-ika.firebaseio.com",
+  storageBucket: "lgtm-ika.appspot.com",
+  messagingSenderId: "991109471676"
+};
